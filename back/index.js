1cfb6a274d9f70e8129ec1c6344c0d3ce0f4975c
José Luis Santana <yuniorglez@Mac-Yunior.local>
2018-03-22 09:52:45 +0000

    Refactoring

--- back/index.js
+++ back/index.js
@@ -1,48 +1,13 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const cors = require('cors');
 const todosRouter = require('./api/TODOS');
-// const todos = JSON.parse(fs.readFileSync('data/todos.json'));
-
-
-
 
 app.use(express.json());
 app.use(cors());
-app.use( (req, res, next) => {
-    console.log(`Yo me ejecuto primero`);
-    req.invalidUser = true;
-    next();
-})
-app.use( (req, res, next) => {
-    console.log(`Y luego yo`);
-    if (req.invalidUser) return res.sendStatus(401)
-    else    next()
-})
-// app.use('/api/users', usersRouter);
+
 app.use('/api/todos', todosRouter);
 
 app.listen(5000, (err) => {
     console.log('Servidor listo en el puerto 5000');
 })
-
-
-
-const functions = require('./utils/functions');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
